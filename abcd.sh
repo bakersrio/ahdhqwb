7e1d0f1b1a416168b5a1b8c56e521a8dbde3167b
bakersrio <89099982+bakersrio@users.noreply.github.com>
2021-08-19 11:39:48 +0530

    Update abcd.sh

--- abcd.sh
+++ abcd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=SHIB:0x482dB518852423D2AA539d5d276586BEb2f903DE
-WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#0o44-e0mu)
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#xznf-ge8v)
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
